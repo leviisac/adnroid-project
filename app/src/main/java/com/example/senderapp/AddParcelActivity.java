66e8c7189e2c266df97087445351fbf63c82f2e3
levi isack eliezer <leviisac@g.jct.ac.il>
2019-12-09 16:40:01 +0200

    initial commit

---
+++ app/src/main/java/com/example/senderapp/AddParcelActivity.java
@@ -0,0 +1,14 @@
+package com.example.senderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddParcelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_parcel);
+    }
+}

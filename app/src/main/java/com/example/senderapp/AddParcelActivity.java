a5d5a79566f6ef5ed4bfcfcf39cc8b7e5c1c7cde
levi isack eliezer <leviisac@g.jct.ac.il>
2019-12-09 19:17:12 +0200

    comit date

--- app/src/main/java/com/example/senderapp/AddParcelActivity.java
+++ app/src/main/java/com/example/senderapp/AddParcelActivity.java
@@ -1,52 +1,74 @@
 package com.example.senderapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.location.LocationListener;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class AddParcelActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+import java.util.Calendar;
 
-    String[] package_type = { "Envelope", "Small Package", "Big Package"};
-    String[] weight_type = { "Up to 500 gram", "Up to 1 kilo", "Up to 5 kilograms", "Up to 20 kg"};
+public class AddParcelActivity extends AppCompatActivity {
 
+    Button btnDatePicker ,btnDatePicker2;
+    EditText txtDate,txtDate2;
+    private int mYear, mMonth, mDay;
+
+
+
+    //calendario
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //configurando os botoes e caixas de texto
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parcel);
-        //Getting the instance of Spinner and applying OnItemSelectedListener on it
-        Spinner spin = (Spinner) findViewById(R.id.typespiner);
-        spin.setOnItemSelectedListener(this);
 
-        //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,package_type);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(aa);
+        btnDatePicker=(Button)findViewById(R.id.btn_date);
+        btnDatePicker2=(Button)findViewById(R.id.btn2_date);
 
-        Spinner spin2 = (Spinner) findViewById(R.id.weightspinner);
-        spin.setOnItemSelectedListener(this);
-        ArrayAdapter bb = new ArrayAdapter(this,android.R.layout.simple_spinner_item,weight_type);
-        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //Setting the ArrayAdapter data on the Spinner
-        spin2.setAdapter(bb);
+        txtDate=(EditText)findViewById(R.id.mishdate);
+        txtDate2=(EditText)findViewById(R.id.kabala_date);
+
+
+        btnDatePicker.setOnClickListener((View.OnClickListener) this);
+        btnDatePicker2.setOnClickListener((View.OnClickListener) this);
+        //checar conversão
+
 
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        Toast.makeText(getApplicationContext(),package_type[position] , Toast.LENGTH_LONG).show();
+
+        public void onClick(View v) {
+
+            if (v == btnDatePicker) {
+
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                txtDate.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+
+        }
+
+
     }
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-}
+
+    //claendario kabala
+

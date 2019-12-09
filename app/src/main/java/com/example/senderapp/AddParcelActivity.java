9763ab5add7170a0edfce5439179a29212a1f7ab
lancry <eliezerlancry@hotmail.com>
2019-12-09 18:45:34 +0200

    Spinners

--- app/src/main/java/com/example/senderapp/AddParcelActivity.java
+++ app/src/main/java/com/example/senderapp/AddParcelActivity.java
@@ -1,14 +1,51 @@
 package com.example.senderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
-public class AddParcelActivity extends AppCompatActivity {
+public class AddParcelActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String[] package_type = { "Envelope", "Small Package", "Big Package"};
+    String[] weight_type = { "Up to 500 gram", "Up to 1 kilo", "Up to 5 kilograms", "Up to 20 kilograms"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parcel);
+        //Getting the instance of Spinner and applying OnItemSelectedListener on it
+        Spinner spin = (Spinner) findViewById(R.id.typespiner);
+        spin.setOnItemSelectedListener(this);
+
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,package_type);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin.setAdapter(aa);
+
+        Spinner spin2 = (Spinner) findViewById(R.id.weightspinner);
+        spin.setOnItemSelectedListener(this);
+        ArrayAdapter bb = new ArrayAdapter(this,android.R.layout.simple_spinner_item,weight_type);
+        bb.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        spin2.setAdapter(bb);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        Toast.makeText(getApplicationContext(),package_type[position] , Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }

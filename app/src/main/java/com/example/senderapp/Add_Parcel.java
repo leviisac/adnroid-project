5e6a6ccee891213b232e721535fd40d654c14b40
lancry <eliezerlancry@hotmail.com>
2019-12-17 12:26:37 +0200

    test

--- app/src/main/java/com/example/senderapp/Add_Parcel.java
+++ app/src/main/java/com/example/senderapp/Add_Parcel.java
@@ -23,6 +23,9 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.List;
@@ -49,6 +52,17 @@ public class Add_Parcel extends AppCompatActivity implements LocationListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__parcel);
 
+        //////////////////FIreBase
+
+       /// FirebaseDatabase database = FirebaseDatabase.getInstance();
+       // DatabaseReference myRef = database.getReference("message");
+
+       // myRef.setValue("Hello, World!");
+
+
+
+
+        ///////////////////
 
         btnDatePicker = (Button) findViewById(R.id.btn_date);
         txtDate = (EditText) findViewById(R.id.in_date);
@@ -72,8 +86,35 @@ public class Add_Parcel extends AppCompatActivity implements LocationListener {
 
 
         //LOCATION CONFIGURATIONS
+        /*
+        Location mobileLocation;
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(Add_Parcel.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Add_Parcel.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Add_Parcel.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        } else {
+        mobileLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Geocoder geocoder;
+            List<Address> addresses = null;
+            geocoder = new Geocoder(this, Locale.getDefault());
 
+            try {
+                addresses = geocoder.getFromLocation(mobileLocation.getLatitude(), mobileLocation.getLongitude(), 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            String city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            String country = addresses.get(0).getCountryName();
+            String postalCode = addresses.get(0).getPostalCode();
+            String knownName = addresses.get(0).getFeatureName(); // Only if available else return NULL
+
+            TextView textViewToChange = (TextView) findViewById(R.id.location);
+            textViewToChange.setText(
+                    city + "  ," + state + "  ," + country + "  ," + postalCode);
+        }
         if (ActivityCompat.checkSelfPermission(Add_Parcel.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(Add_Parcel.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(Add_Parcel.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
             return;
@@ -83,6 +124,7 @@ public class Add_Parcel extends AppCompatActivity implements LocationListener {
                     0, this);
 
         }
+    */
 
 
     }
@@ -90,9 +132,7 @@ public class Add_Parcel extends AppCompatActivity implements LocationListener {
     @Override
     public void onLocationChanged(Location location) {
 
-        //String str = "Latitude: "+location.getLatitude()+"\nLongitude: "+location.getLongitude();
 
-        //Toast.makeText(getBaseContext(), str, Toast.LENGTH_LONG).show();
 
 
         //////////////////////////////////
@@ -116,7 +156,7 @@ public class Add_Parcel extends AppCompatActivity implements LocationListener {
 
         TextView textViewToChange = (TextView) findViewById(R.id.location);
         textViewToChange.setText(
-                city + " ," + state + " ," + country + " ," + postalCode);
+                city + "  ," + state + "  ," + country + "  ," + postalCode);
         //////////////////////////////
     }
 
--- app/src/main/java/com/example/senderapp/MainActivity.java
+++ app/src/main/java/com/example/senderapp/MainActivity.java
@@ -7,12 +7,22 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
